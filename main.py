e2df61d4bd308667b34685478d40bdb9783a8d6f
Antoine <caillonantoine@gmail.com>
2018-01-29 19:45:55 +0100

    Ajout de la fonction green, stabilisation du code, ajout de commentaires

--- main.py
+++ main.py
@@ -3,44 +3,63 @@ import numpy as np
 from scipy.special import hankel1
 import matplotlib.pyplot as plt
 
-def d(x,y):
-    z = np.zeros(len(x))
-    for i in range(len(x)):
-        z[i] = np.sqrt(np.power(x[i][0] - y[0],2) + np.power(a,b)(x[i][1] - y[1],2))
-    return z
-
 def show():
-    global points,s0,s1
+    """Affiche la configuration"""
+    global points,s0
     plt.plot(points[:,0],points[:,1])
     plt.axis('equal')
     plt.scatter(*s0)
-    for elm in s1:
-        plt.scatter(*elm)
+    for passe in si:
+        for elm in passe:
+            plt.scatter(*elm)
+    plt.grid()
         
 def sym(s,a,b):
+    """Donne les coordonnées du symétrique d'un point s par rapport au segment [a,b]"""
     s,a,b = np.asarray(s),np.asarray(a),np.asarray(b)
     h = (np.dot(s-a,b-a)/np.linalg.norm(b-a)**2)*(b-a) + a
-    return 2*h -s
-    
-    
-H1 = lambda z:hankel1(0,z) #Définition de la fonction de Hankel de première espèce et d'ordre 0
+    return (2*h -s)
 
-G = lambda r,r0,k0: (1/4j)*H1(k0*d(r,r0)) #Définition de la fonction de Green en dimension 2
+def Green(r,k0):
+    global si
+    y = 0
+    for i in range(1,len(si)):
+        for elm in si[i]:
+            y += G(r,np.array(elm),k0)
+    return y
+
+H1 = lambda z:hankel1(0,z)
+
+G = lambda r,r0,k0: (1/4j)*H1(k0*np.linalg.norm(r-r0))
 
 
-points = np.asarray([[0,0],[1,0],[.5,1],[0,1]])
-plans = np.asarray([[i%4,(i+1)%4] for i in range(4)])
+points = np.asarray([[i,.5*np.sin(3*i)] for i in np.linspace(0,5,10)]) #Définition des points de la frontière
+plans = np.asarray([[i,(i+1)] for i in range(9)]) #Définition des segments entre chaque points
 
-s0 = np.asarray([.5,.5])
-si = [[s0]]
-#%%
-for i in range(2):
-    si.append([])
-    for j,elm in enumerate(plans):
-        for source in si[i]:
-            a,b = points[elm[0]], points[elm[1]]
-            u = b-a
-            sa = source-a
-            proj = np.dot(sa,u)/np.linalg.norm(u)
-            if proj >= 0 and proj <= 1:
-                si[i+1].append(sym(s0,a,b))
+s0 = np.asarray([2.5,2]) #Positionnement de la source
+
+n = 2 #Nombre de passes de réflexion
+si = [0 for i in range(n)] #Liste de liste de points
+si[0] = [s0]
+
+for i in range(1,n): #Calcul des sources n-ième
+    si[i] = []
+    for source in si[i-1]:
+        for plan in plans:
+            si[i].append(sym(source, points[plan[0]],points[plan[1]]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
