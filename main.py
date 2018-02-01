48338cce398ae0726ead525f9cfef376b6d46d7c
Antoine <caillonantoine@gmail.com>
2018-02-01 17:10:10 +0100

    Ajout des sources cpp, link avec floyd, réecriture du programme depuis python

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ def show():
     global points,s0
     plt.plot(points[:,0],points[:,1])
     plt.axis('equal')
+    #plt.axis([-2,7,-4.5,4.5])
     plt.scatter(*s0)
     for passe in si:
         for elm in passe:
             plt.scatter(*elm)
     plt.grid()
+    plt.show()
         
 def sym(s,a,b):
     """Donne les coordonnées du symétrique d'un point s par rapport au segment [a,b]"""
@@ -21,6 +23,7 @@ def sym(s,a,b):
     return (2*h -s)
 
 def Green(r,k0):
+    """Fonction de green correspondant à toutes les sources images"""
     global si
     y = 0
     for i in range(1,len(si)):
@@ -32,13 +35,13 @@ H1 = lambda z:hankel1(0,z)
 
 G = lambda r,r0,k0: (1/4j)*H1(k0*np.linalg.norm(r-r0))
 
+N = 4
+points = np.asarray([[0,0],[1,0],[1,1],[0,1]]) #Définition des points de la frontière
+plans = np.asarray([[i%N,(i+1)%N] for i in range(N)]) #Définition des segments entre chaque points
 
-points = np.asarray([[i,.5*np.sin(3*i)] for i in np.linspace(0,5,10)]) #Définition des points de la frontière
-plans = np.asarray([[i,(i+1)] for i in range(9)]) #Définition des segments entre chaque points
+s0 = np.asarray([.25,.5]) #Positionnement de la source
 
-s0 = np.asarray([2.5,2]) #Positionnement de la source
-
-n = 2 #Nombre de passes de réflexion
+n = 3 #Nombre de passes de réflexion
 si = [0 for i in range(n)] #Liste de liste de points
 si[0] = [s0]
 
@@ -46,16 +49,11 @@ for i in range(1,n): #Calcul des sources n-ième
     si[i] = []
     for source in si[i-1]:
         for plan in plans:
-            si[i].append(sym(source, points[plan[0]],points[plan[1]]))
+            symetrique = sym(source, points[plan[0]],points[plan[1]])
+            if not (True in [(symetrique == elm).all() for elm in si[i-1]]):
+                si[i].append(symetrique)
 
-
-
-
-
-
-
-
-
+#%% BEM
 
 
 
