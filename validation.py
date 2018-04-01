61904561e6a33290065129d4a33f1bc814ab4188
Antoine <caillonantoine@gmail.com>
2018-04-01 19:34:14 +0200

    Ajout deuxième configuration de validation

--- validation.py
+++ validation.py
@@ -1,40 +1,32 @@
 #coding: utf-8
 import numpy as np
-from modules import rough_interface as ri
+import rough_interface as ri
+from rough_interface import geometry
+import matplotlib.pyplot as plt
 
 lam = 1
 
 problem = ri.RoughInterfaceScattering()
-trou= .3*lam
 
-e =  .25*lam
-f = e + 1.5*lam
-g = .5*lam + f
-h = 50
-
-d,c,b,a = -e,-f,-g,-h
-
-points = zip(np.concatenate([np.linspace(a,b,500),\
-						 np.linspace(c,d,100),\
-						 np.linspace(e,f,100),\
-						 np.linspace(g,h,500)]),np.zeros(1200))
-elements = np.concatenate([[(i,i+1) for i in range(499)],\
-							[(i,i+1) for i in range(500,599)],\
-							[(i,i+1) for i in range(600,699)],\
-							[(i,i+1) for i in range(700,1199)]])
+points,elements = geometry.import_gmsh('/home/antoine/untitled.msh')
 
 points = np.asarray(points)
 elements = np.asarray(elements)
 
 
 problem.set_interface(points,elements)
-problem.set_source('plane',[0,5*lam])
+problem.set_source('ponctuelle',[-5,5])
 problem.set_angle(0)
-problem.set_omega([-10*lam,10*lam,-10*lam,10*lam],150,.5)
+problem.set_omega([-10*lam,10*lam,-10*lam,10*lam],150,.1)
 problem.set_frequency(lam*340)
 problem.set_circle(2,[0,1])
 
-pression, directivite = problem.start(quiet = True)
+problem.n[167:189] *= -1
+problem.n[240:262] *= -1
+problem.n[275:297] *= -1
+problem.n[346:368] *= -1
+
+pression, directivite = problem.start()
 
 #%%
 import matplotlib.pyplot as plt
@@ -46,7 +38,7 @@ plt.savefig('validation_ris.eps')
 
 #%%
 
-ligne = zip(np.linspace(-10,10,1000),-5*np.ones(1000))
+ligne = zip(np.linspace(-10,10,1000),7.5*np.ones(1000))
 
 problem.cercle = ligne
 p,l = problem.start(quiet=True)
